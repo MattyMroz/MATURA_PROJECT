7e5542c5b2aec6fab35a2fd95756862f4bda092b
Mateusz Mróz <71221358+MattyMroz@users.noreply.github.com>
2023-02-07 14:54:01 +0100

    ok

--- SPRAWDZIAN_1/zadania.py
+++ SPRAWDZIAN_1/zadania.py
@@ -1,24 +1,3 @@
-# Dzielniki liczby
-# def is_prime(n):
-#     if n < 2:
-#         return False
-#     for i in range(2, int(n**0.5)+1):
-#         if n % i == 0:
-#             return False
-#     return True
-
-# def rozklad(n):
-#     rozklad = []
-#     for i in range(2, int(n**0.5)+1):
-#         if n % i == 0:
-#             if is_prime(i):
-#                 rozklad.append(i)
-#             if is_prime(n // i):
-#                 rozklad.append(n // i)
-#     rozklad.sort()
-#     return rozklad
-
-
 import random
 
 # Zadanie 1
